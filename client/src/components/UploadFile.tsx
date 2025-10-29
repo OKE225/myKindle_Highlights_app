0fcbef403be0ad4488d176c05d5d8ca962957de8
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-10-29 20:27:30 +0100

    Fetch file with highlights, send to backend, and receive parsed data

--- client/src/components/UploadFile.tsx
+++ client/src/components/UploadFile.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import { useRef } from "react";
 
 const UploadFile = () => {
-  return <div>UploadFile</div>;
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleClick = () => {
+    if (inputRef.current) {
+      inputRef.current.click();
+    }
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const file = e.target.files[0];
+
+      const formData = new FormData();
+      formData.append("highlights_file", file);
+
+      fetch("http://localhost:5001/upload", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.error("Error uploading file:", error));
+    }
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        accept=".txt"
+        className="hidden"
+        ref={inputRef}
+        onChange={handleChange}
+      />
+      <button
+        className="bg-brown-900 text-brown-50 px-5 py-3 m-10 text-lg rounded-sm cursor-pointer"
+        onClick={handleClick}>
+        Upload File
+      </button>
+    </div>
+  );
 };
 
 export default UploadFile;
