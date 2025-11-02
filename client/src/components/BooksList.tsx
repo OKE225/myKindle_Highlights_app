1467c7f81897dd0c0d9abde120a9122d0e24f7bd
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-11-02 11:25:32 +0100

    Check if any books exist

--- client/src/components/BooksList.tsx
+++ client/src/components/BooksList.tsx
@@ -9,11 +9,15 @@ const BooksList = () => {
   return (
     <>
       <h2 className="text-4xl">Your books</h2>
-      <div className="grid grid-cols-4 gap-2 m-5">
-        {allBooksList.map((book: Book) => (
-          <BookCard key={book.title} book={book} />
-        ))}
-      </div>
+      {allBooksList.length > 0 ? (
+        <div className="grid grid-cols-4 gap-2 m-5">
+          {allBooksList.map((book: Book) => (
+            <BookCard key={book.title} book={book} />
+          ))}
+        </div>
+      ) : (
+        <p>You don't have any books</p>
+      )}
     </>
   );
 };
