5120abddd261c2382b338df4cd5ff9bdb873560b
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-11-04 08:24:50 +0100

    Improve highlight card design

--- client/src/components/Highlight.tsx
+++ client/src/components/Highlight.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Highlight = ({ info: { metadata, text } }: Props) => {
   return (
-    <div className="bg-brown-100 p-5 rounded-sm">
+    <div className="bg-stone-50 shadow p-5 rounded-sm">
       <h4 className="text-2xl mb-1">{text}</h4>
       <p className="text-zinc-700">{metadata}</p>
     </div>
