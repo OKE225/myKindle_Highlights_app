15a28227a2bff843d6b30a44867afa622155afda
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-11-05 22:36:02 +0100

    Code changed

--- client/src/components/HighlightCard.tsx
+++ client/src/components/HighlightCard.tsx
@@ -1,11 +1,9 @@
 interface Props {
-  info: {
-    metadata: string;
-    text: string;
-  };
+  text: string;
+  metadata: string;
 }
 
-const HighlightCard = ({ info: { metadata, text } }: Props) => {
+const HighlightCard = ({ metadata, text }: Props) => {
   return (
     <div className="bg-stone-50 shadow p-5 rounded-sm">
       <h4 className="text-2xl mb-1">{text}</h4>
--- client/src/components/HighlightsList.tsx
+++ client/src/components/HighlightsList.tsx
@@ -2,17 +2,21 @@ import HighlightCard from "./HighlightCard";
 import HighlightsCount from "./HighlightsCount";
 
 interface Props {
-  highlights: [];
+  highlights: { text: string; metadata: string }[];
 }
 
 const HighlightsList = ({ highlights }: Props) => {
   return (
     <div>
       <HighlightsCount count={highlights.length} />
-      <hr className="my-5" />
+      <hr className="mb-5" />
       <div className="grid grid-cols-2 gap-1">
         {highlights.map((highlight) => (
-          <HighlightCard info={highlight} />
+          <HighlightCard
+            key={highlight.metadata}
+            text={highlight.text}
+            metadata={highlight.metadata}
+          />
         ))}
       </div>
     </div>
