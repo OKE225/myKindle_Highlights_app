01a5eb578c3287c589d645703df5fda2906858a1
OKE225 <patryk.wroblewski.2006@gmail.com>
2025-11-05 22:33:36 +0100

    Add loader when data fetching

--- client/src/components/BooksList.tsx
+++ client/src/components/BooksList.tsx
@@ -4,19 +4,22 @@ import { AppContext } from "../AppContext";
 import type Book from "../types/Book";
 
 const BooksList = () => {
-  const { allBooksList } = useContext(AppContext);
+  const { allBooksList, fetchIsLoading } = useContext(AppContext);
 
   return (
-    <div className="mb-15">
+    <div className="mb-10">
       <h2 className="text-4xl">Your books</h2>
       {allBooksList.length > 0 ? (
-        <div className="grid grid-cols-4 gap-2 mt-5">
+        <div className="grid grid-cols-4 gap-2 mt-2">
           {allBooksList.map((book: Book) => (
             <BookCard key={book.title} book={book} />
           ))}
         </div>
       ) : (
-        <p>You don't have any books</p>
+        <>
+          <p>You don't have any books</p>
+          {fetchIsLoading && <div className="loader bg-stone-600 mt-5"></div>}
+        </>
       )}
     </div>
   );
